0cd03ff78e6e15d8532e5ca0b26412d9ce1c18ef
lemmonie <joycelyntsang@gmail.com>
2026-04-02 02:58:53 -0700

    finally finish debug in initial stage

--- backend/src/email.js
+++ backend/src/email.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
   },
-  famly : 4,
+  family : 4,
 })
 
 const sendEmail = async ({ to, subject, html }) => {
