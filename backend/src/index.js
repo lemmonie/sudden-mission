677b90c70096ece7ae6d4de56a6a90f14386b440
lemmonie <joycelyntsang@gmail.com>
2026-04-01 23:55:29 -0700

    email problem

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,10 @@ io.on('connection', (socket) => {
   })
 })
 
-app.use(cors())
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}))
 app.use(express.json())
 
 app.use('/api/auth', googleAuthRoutes)
