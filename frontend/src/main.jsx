4d8cd90da5d3f286efb327e77d4fb0eb43d6c47c
lemmonie <joycelyntsang@gmail.com>
2026-04-28 21:35:44 -0700

    change README

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles/global.css'
 import App from './App.jsx'
 
+// the main root of the webpage, rendering the entire app inside StrictMode for better development checks
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
